f90a853faa138468cd8afdb84e20011efb138875
framorgi <francesco.morgillo@hotmail.com>
2025-12-24 16:21:17 +0000

    cmake settings

---
+++ cmake/Options.cmake
@@ -0,0 +1,6 @@
+# #############################################################################################
+# Contains all the options that can beset to change the build configuration
+# #############################################################################################
+option(ENABLE_UTESTS "Enable building of unit tests" OFF) #TODO: Change to ON to enable tests by default
+option(ENABLE_DOXYGEN "Enable generation of Doxygen documentation" OFF)
+option(EBABLE_CODE_FORMATTING "Enable code formatting using clang-format" ON)
